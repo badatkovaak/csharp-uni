cbd8b449f8437a9558806a98ee411d8cba60a5c4
badatkovaak <daniil.daniil9779@gmail.com>
2025-03-27 19:35:03 +0300

    1

--- Program.cs
+++ Program.cs
@@ -18,20 +18,34 @@
         /*Complex y = new Complex(Math.Sqrt(2)/2, Math.Sqrt(2)/2);*/
         /*Console.WriteLine((++y).Abs());*/
 
-        (double, double)[] points1 = { (10, 165), (11, 220), (12, 286), (13, 364) };
-        Polynomial p1 = Polynomial.constructLagrangePolynomial(points1);
-        Console.WriteLine(p1);
+        /*(double, double)[] points1 = { (10, 165), (11, 220), (12, 286), (13, 364) };*/
+        /*Polynomial p1 = Polynomial.constructLagrangePolynomial(points1);*/
+        /*Console.WriteLine(p1);*/
+        /**/
+        /*(double, double)[] points2 =*/
+        /*{*/
+        /*    (4, 36),*/
+        /*    (5, 126),*/
+        /*    (6, 336),*/
+        /*    (7, 756),*/
+        /*    (8, 1512),*/
+        /*    (9, 2772),*/
+        /*};*/
+        /*Polynomial p2 = Polynomial.constructLagrangePolynomial(points2);*/
+        /*Console.WriteLine(p2);*/
 
-        (double, double)[] points2 =
-        {
-            (4, 36),
-            (5, 126),
-            (6, 336),
-            (7, 756),
-            (8, 1512),
-            (9, 2772),
-        };
-        Polynomial p2 = Polynomial.constructLagrangePolynomial(points2);
-        Console.WriteLine(p2);
+        Expression e2 = new BinaryExpression(
+            Operation.Multiplication,
+            new Number(3),
+            new Number(4)
+        );
+        Expression e = new BinaryExpression(Operation.Addition, e2, new Number(2));
+        Console.WriteLine(e);
+        Calculator c = new Calculator(e, 0);
+        c.add_operation(Operation.Addition, 5);
+        c.add_operation(Operation.Addition, 6);
+        c.add_operation(Operation.Multiplication, 7);
+        c.add_operation(Operation.Addition, 8);
+        Console.WriteLine(c.expression);
     }
 }
